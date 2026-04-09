f2ed91d8a6867e0519bf1b3dcccd225a580800d8
Yarynka <jaryna.zhvanko@gmail.com>
2026-04-09 19:49:32 +0300

    That is all in my opinion

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+from sympy import randprime
+import json
+import hashlib
 
 class Client:
     def __init__(self, server_ip: str, port: int, username: str) -> None:
@@ -7,47 +10,75 @@ class Client:
         self.port = port
         self.username = username
 
+        #these variables were added by me
+        self.n=None
+        self.d=None
+
+        self.server_n=None
+        self.server_e=None
     def init_connection(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.s.connect((self.server_ip, self.port))
         except Exception as e:
             print("[client]: could not connect to server: ", e)
-            return
+            return None
 
         self.s.send(self.username.encode())
 
         # create key pairs
+        p1 = randprime(2**512, 2**1024)
+        p2 = randprime(2**512, 2**1024)
+        n=p1*p2
+        phi=(p1-1)*(p2-1)
 
+        e=65537 #спершу я теж через рандпрайм рахувала, але я прочитала що це стандартне число і так швижше працює алгоритм
+        d=pow(e, -1, phi)
+
+        self.n=n
+        self.d=d
         # exchange public keys
-
+        public={'e':e, 'n':n}
+        json_data = json.dumps(public)
+        self.s.send(json_data.encode())
         # receive the encrypted secret key
+        server_public_key = json.loads(self.s.recv(1024).decode())
+        self.server_n=server_public_key['n']
+        self.server_e=server_public_key['e']
 
         message_handler = threading.Thread(target=self.read_handler,args=())
         message_handler.start()
         input_handler = threading.Thread(target=self.write_handler,args=())
         input_handler.start()
 
-    def read_handler(self): 
+    def read_handler(self):
         while True:
-            message = self.s.recv(1024).decode()
+            message = self.s.recv(2048) # розмір в два рази більший бо приймається і хеш і саме повідомлення, не знаю чи мала я право його змінювати
+            if not message:
+                break
+            packet = json.loads(message.decode())
+            hashh = packet['hash']
+            msg = packet['encrypted_message']
 
-            # decrypt message with the secrete key
+            decrypted_int = pow(int(msg), self.d, self.n)
+            decrypted = decrypted_int.to_bytes((decrypted_int.bit_length() + 7) // 8, 'big').decode()
+            current_hash = hashlib.sha256(str(decrypted).encode()).hexdigest()
 
-            # ... 
-
-
-            print(message)
+            if current_hash == hashh:
+                print(decrypted)
+            else:
+                print("!!! ПОПЕРЕДЖЕННЯ: Повідомлення було змінено або пошкоджено !!!")
 
     def write_handler(self):
         while True:
             message = input()
-
-            # encrypt message with the secrete key
-
-            # ...
-
-            self.s.send(message.encode())
+            if not message:
+                break
+            hashh = hashlib.sha256(str(message).encode()).hexdigest()
+            msg = int.from_bytes(message.encode(), 'big')
+            encrypted = pow(msg, self.server_e, self.server_n)
+            packet = {'hash': hashh,'encrypted_message': encrypted}
+            self.s.send(json.dumps(packet).encode())
 
 if __name__ == "__main__":
     cl = Client("127.0.0.1", 9001, "b_g")
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+from sympy import randprime
+import json
+import hashlib
 
 class Server:
 
@@ -9,51 +12,82 @@ class Server:
         self.clients = []
         self.username_lookup = {}
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.client_keys = {}
+        self.n=None
+        self.d=None
 
     def start(self):
         self.s.bind((self.host, self.port))
         self.s.listen(100)
 
-        # generate keys ...
+        # create key pairs
+        p1 = randprime(2**512, 2**1024)
+        p2 = randprime(2**512, 2**1024)
+        n=p1*p2
+        phi=(p1-1)*(p2-1)
+
+        e=65537 #спершу я теж через рандпрайм рахувала, але я прочитала що це стандартне число і так швижше працює алгоритм
+        d=pow(e, -1, phi)
+
+        self.n=n
+        self.d=d
 
         while True:
             c, addr = self.s.accept()
             username = c.recv(1024).decode()
             print(f"{username} tries to connect")
-            self.broadcast(f'new person has joined: {username}')
             self.username_lookup[c] = username
-            self.clients.append(c)
 
-            # send public key to the client 
+            # send public key to the client
 
-            # ...
+            public={'e':e, 'n':n}
+            json_data = json.dumps(public)
+            c.send(json_data.encode())
 
             # encrypt the secret with the clients public key
 
-            # ...
+            client_public_key = json.loads(c.recv(1024).decode())
+            self.client_keys[c] = {'e': client_public_key['e'], 'n': client_public_key['n']}
 
-            # send the encrypted secret to a client 
-
-            # ...
+            self.clients.append(c)
+            self.broadcast(f'new person has joined: {username}')
 
             threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
     def broadcast(self, msg: str):
-        for client in self.clients: 
+        hashh = hashlib.sha256(str(msg).encode()).hexdigest()
+        msg_int = int.from_bytes(msg.encode(), 'big')
 
-            # encrypt the message
+        for client in self.clients:
+            client_e = self.client_keys[client]['e']
+            client_n = self.client_keys[client]['n']
 
-            # ...
+            encrypted = pow(msg_int, client_e, client_n)
+            packet = {'hash': hashh, 'encrypted_message': encrypted}
+            client.send(json.dumps(packet).encode())
 
-            client.send(msg.encode())
-
-    def handle_client(self, c: socket, addr): 
+    def handle_client(self, c: socket, addr):
         while True:
             msg = c.recv(1024)
+            if not msg:
+                break
 
+            packet = json.loads(msg.decode())
+            encrypted_int = int(packet['encrypted_message'])
+
+            # 1. Сервер розшифровує своїм ключем
+            decrypted_int = pow(encrypted_int, self.d, self.n)
+            decrypted_str = decrypted_int.to_bytes((decrypted_int.bit_length() + 7) // 8, 'big').decode()
+
+            # 2. Перешифровує для інших клієнтів
             for client in self.clients:
                 if client != c:
-                    client.send(msg)
+                    client_e = self.client_keys[client]['e']
+                    client_n = self.client_keys[client]['n']
+
+                    encrypted_for_client = pow(int.from_bytes(decrypted_str.encode(), 'big'), client_e, client_n)
+                    forward_packet = {'hash': packet['hash'], 'encrypted_message': encrypted_for_client}
+                    client.send(json.dumps(forward_packet).encode())
 
 if __name__ == "__main__":
     s = Server(9001)
